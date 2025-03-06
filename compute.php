535825960e4bfa2375079154520fc65f0cf768e3
Alexej Sidorenko <fra.iesus@gmail.com>
2025-03-06 01:26:18 +0100

    Update compute.php
    time measurement added

--- compute.php
+++ compute.php
@@ -1,33 +1,66 @@
 <?php
 $knownResults = [
-    0 => 1,
-    1 => 1,
-    2 => 2
+  0 => 1,
+  1 => 1,
+  2 => 2,
 ];
 
 function getFactorial($n) {
     global $knownResults;
 
+    $start = microtime(true);
+
     if (isset($knownResults[$n])) {
-        return $knownResults[$n];
+        $result = $knownResults[$n];
+        $source = 'cache';
+    } else {
+        $result = gmp_intval(gmp_fact($n));
+        $knownResults[$n] = $result;
+        $source = 'calculation';
+
+        $code = file_get_contents(__FILE__);
+
+        if ($code === false) {
+            echo "Failed to read the file!\n";
+            return;
+        }
+        
+        $updatedKnownResults = var_export($knownResults, true);
+        $updatedKnownResults = preg_replace('/^array\s*\(/', '', $updatedKnownResults);
+        $updatedKnownResults = preg_replace('/\);$/', '', $updatedKnownResults);
+
+        if (substr($updatedKnownResults, -1) === ')') {
+            $updatedKnownResults = substr($updatedKnownResults, 0, -1);
+        }
+
+        $updatedCode = preg_replace(
+            '/\$knownResults\s*=\s*\[.*?\];/s',
+            '$knownResults = [' . $updatedKnownResults . '];',
+            $code,
+            1
+        );
+
+        if ($updatedCode === $code) {
+            echo "No change in code, \$knownResults not updated.\n";
+        } else {
+            echo "Code was updated.\n";
+            file_put_contents(__FILE__, $updatedCode);
+        }
     }
 
-    $result = gmp_intval(gmp_fact($n));
-    $knownResults[$n] = $result;
+    $end = microtime(true);
+    $duration = round(($end - $start) * 1000, 3);
 
-    $code = file_get_contents(__FILE__);
-    $updatedCode = preg_replace(
-        '/\$knownResults = .*?;\n/s',
-        '$knownResults = ' . var_export($knownResults, true) . ";\n",
-        $code
-    );
-
-    file_put_contents(__FILE__, $updatedCode);
+    echo "Result: $result (from $source) in $duration ms\n";
 
     return $result;
 }
 
-# Test
-echo getFactorial(5) . PHP_EOL;
-echo getFactorial(5) . PHP_EOL;
+# Tests
+getFactorial(5);
+getFactorial(5);
+getFactorial(10);
+getFactorial(10);
+getFactorial(20);
+getFactorial(20);
 ?>
